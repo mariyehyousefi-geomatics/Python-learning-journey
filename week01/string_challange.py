10de0b47a199e1a6474326ca689995031b228e7f
mariyehyousefi-a11y <mariyeh.yousefi@gmail.com>
2025-12-06 15:45:09 +0330

    Add files via upload

---
+++ week01/string_challange.py
@@ -0,0 +1,6 @@
+full_text="Python programming is interesting and powerful"
+print(full_text.split()[2])
+print(full_text[38])
+print(full_text.replace("i","*"))
+print(len(full_text))
+print(f"The scentence has {len(full_text)} characters and its last word is '{full_text.split()[-1]}'")
---
+++ week01/variables.py
@@ -0,0 +1,5 @@
+name= "Mariyeh"
+age= 42
+height= 1.71
+sports= True
+print(name,age,height,sports)
